42449ca6e671a898995d42b61470c6965ea66603
Jordan Lewis <jordan@knewton.com>
2014-05-13 20:16:41 -0400

    add roud and recorded by

--- parse.py
+++ parse.py
@@ -8,6 +8,8 @@ filename_regex = re.compile("^filename", re.I)
 paren_regex = re.compile("[()]")
 child_regex = re.compile("^child #(.*)", re.I)
 laws_regex = re.compile("^Laws (.*)")
+roud_regex = re.compile("^Roud(?:-| #| R)(.*)")
+recorded_by_regex = re.compile("^recorded by (.*)", re.I)
 
 # our final data dictionary, will be written to data.json
 data = {}
@@ -29,34 +31,38 @@ for filename in os.listdir("mudcat"):
     for idx, line in enumerate(lines):
         if idx == 0:
             song["title"] = line.strip()
-            continue
         elif not found_break:
             if line:
                 # this is sometimes the author of the lyrics in parens
                 author_lines.append(paren_regex.sub("", line.strip()))
             else:
                 found_break = True
-            continue
         elif line.startswith("<A "):
             continue
-        # run capturing matches
-        child_match = child_regex.match(line)
-        laws_match = laws_regex.match(line)
-        if filename_regex.match(line):
-            song["name"] = line.split("[")[1].strip()
         elif line.startswith("@"):
             song["tags"] = [tag.strip() for tag in line.split("@")[1:]]
+        elif line.startswith("DT #"):
+            continue
         elif line.startswith("TUNE FILE: "):
             song["tune"] = line[11:]
-        elif child_match:
-            song["chld"] = child_match.group(1)
-        elif laws_match:
-            song["laws"] = laws_match.group(1)
-        elif line.startswith("DT #"):
-            print line
-            continue
+        elif filename_regex.match(line):
+            song["name"] = line.split("[")[1].strip()
         else:
-            lyrics.append(line)
+            # run capturing matches
+            child_match = child_regex.match(line)
+            laws_match = laws_regex.match(line)
+            recorded_by_match = recorded_by_regex.match(line)
+            roud_match = roud_regex.match(line)
+            if child_match:
+                song["chld"] = child_match.group(1)
+            elif laws_match:
+                song["laws"] = laws_match.group(1)
+            elif recorded_by_match:
+                song["rec"] = recorded_by_match.group(1)
+            elif roud_match:
+                song["roud"] = roud_match.group(1)
+            else:
+                lyrics.append(line)
     song["txt"] = lyrics
     if author_lines:
         song["a"] = " ".join(author_lines)
