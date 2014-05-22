df20265577f4b3f90063456309e01ca822c0f7e7
Jordan Lewis <jordan@knewton.com>
2014-05-22 12:35:53 -0400

    Use native js objects for main data storage
    It was slow to convert the big json object into clojurescript, so we'll
    just keep it in a big js object for now.

--- src/dtbrowser/core.cljs
+++ src/dtbrowser/core.cljs
@@ -4,25 +4,46 @@
             [om.dom :as dom :include-macros true]
             [clojure.string :refer [split join capitalize]]
             [cljs.core.async :refer [put! chan <!]]
-            [ajax.core :refer [GET]]))
+            [ajax.core :refer [GET]])
+  (:import [goog.net XhrIo]
+           goog.object
+           goog.net.EventType
+           [goog.events EventType]))
 
 (enable-console-print!)
 
+;; the song data will go in here after fetching
 (def data {})
+(def all-items [])
 
 (def app-state (atom {:items [] :selected {} :loading true}))
 
-(def index (js/lunr #(this-as this (. this field "title") (. this ref "id"))))
+;; our lunr.js index
+(def index (js/lunr #(this-as this
+                              (. this field "title")
+                              (. this ref "id"))))
 
+;; gets called when the user changes the filter text
+;; searches the index and sets :selected to the result
 (defn handle-filter-change [e app owner {:keys [filter-text]}]
   (let [new-filter-text (.. e -target -value)
         found-items (if (empty? new-filter-text)
-                      (keys data)
-                      (map #(.-ref %) (. index search new-filter-text)))]
+                      all-items
+                      (into [] (map #(.-ref %) (. index search new-filter-text))))]
     (prn new-filter-text)
     (om/set-state! owner :filter-text new-filter-text)
     (swap! app-state assoc :items found-items)))
 
+;; a component for a single song in the list
+(defn song-view [songid owner]
+  (reify
+    om/IRender
+    (render [this]
+      (dom/li nil
+        (dom/a #js {:onClick (fn [e] (swap! app-state assoc :selected songid))}
+               (aget (aget data songid) "title"))))))
+
+;; a component for a list of songs
 (defn song-list-view [app owner]
   (reify
     om/IShouldUpdate
@@ -32,12 +53,9 @@
     (render [this]
       (prn "rerendering")
       (apply dom/ul nil
-        (map (fn [[k v]]
-               (dom/li nil
-                 (dom/a #js {:onClick (fn [e] (om/update! app :selected k))}
-                        (v "title"))))
-             (select-keys data (:items app)))))))
+        (om/build-all song-view (if-let [items (:items app)] items (js/Object.keys data)))))))
 
+;; a component for a filterable list of songs
 (defn filterable-song-list-view [app owner]
   (reify
     om/IInitState
@@ -48,7 +66,7 @@
       (dom/div #js {:id "song-list-container"}
         (dom/h2 nil "Song list")
           (if (:loading app)
-            (dom/span nil "Please wait while the index is populated...")
+            (dom/span #js {:id "hodor"} "Please wait while the index is populated...")
             (dom/input #js {:type "text" :ref "filter-text" :value (:filter-text state)
                             :onChange #(handle-filter-change % app owner state)}))
         (om/build song-list-view app)))))
@@ -61,21 +79,29 @@
         (om/build filterable-song-list-view app)
         (dom/div #js {:id "song-view-container"}
           (dom/h2 nil "Song text")
-          (dom/pre nil (join "\n" (get (get data (:selected app) {}) "txt" ""))))))))
-
-(defn titleize [item]
-  (->> "title" item (#(split % " ")) (map capitalize) (join " ") (assoc item "title")))
+            (if-let [song (aget data (:selected app))]
+              (dom/pre nil (join "\n" (aget song "txt")))))))))
 
 (om/root app app-state {:target (. js/document (getElementById "app"))})
 
+(def indexchan (chan))
+
 (defn handler [response]
   (prn "Setting up state...")
-  (set! data (into {} (for [[k v] response] [k (titleize v)])))
-  (swap! app-state assoc :items (keys data))
-  (prn "Building lunr.js index...")
-  (dorun (map (fn [[k v] i]
-                (. index add #js {"id" k "title" (v "title") })) response (range)))
-  (swap! app-state assoc :loading false)
-  (prn "Done"))
+  (set! data (.getResponseJson (.-target response)))
+  (set! all-items (js/Object.keys data))
+  (swap! app-state assoc :items all-items)
 
-(GET "data.json" {:handler handler :response-format :json})
+  (js/setTimeout
+    (fn []
+
+    (prn "Building lunr.js index...")
+
+    (dorun (map (fn [k i]
+                  ;;(set! (.-innerHTML (. js/document (getElementById "hodor"))) "blah")
+                  (. index add #js {"id" k "title" (aget (aget data k) "title")}))
+                (js/Object.keys data) (range)))
+    (swap! app-state assoc :loading false)
+    (prn "Done")) 100))
+
+(.send XhrIo "data.json" handler)
