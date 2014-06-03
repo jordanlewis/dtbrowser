d64ca217eff83e8e910fb88b426852d138e8958e
Jordan Lewis <jordan@knewton.com>
2014-06-02 21:40:27 -0400

    Update library versions

--- project.clj
+++ project.clj
@@ -2,13 +2,13 @@
   :description "Digitrad data browser"
   :url "http://example.com/FIXME"
 
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2173"]
-                 [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2227"]
+                 [org.clojure/core.async "0.1.303.0-886421-alpha"]
                  [cljs-ajax "0.2.3"]
                  [om "0.5.0"]]
 
-  :plugins [[lein-cljsbuild "1.0.2"]]
+  :plugins [[lein-cljsbuild "1.0.3"]]
 
   :source-paths ["src"]
 
